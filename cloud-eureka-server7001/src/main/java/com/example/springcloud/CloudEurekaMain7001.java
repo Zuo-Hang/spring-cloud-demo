11add964c94ae01ed98980b4e64cbf431ec1451f
zuo_h <zuo_hang97@163.com>
2021-04-13 14:08:34 +0800

    整合单机版 eureka

--- cloud-eureka-server7001/src/main/java/com/example/springcloud/CloudEurekaMain7001.java
+++ cloud-eureka-server7001/src/main/java/com/example/springcloud/CloudEurekaMain7001.java
@@ -2,16 +2,16 @@ package com.example.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @Author zuo_h
- * @Date 23:58 2021/4/11
+ * @Date 13:26 2021/4/13
  */
 @SpringBootApplication
-@EnableEurekaClient
-public class OrderMain80 {
+@EnableEurekaServer//代表是eureka的服务注册中心
+public class CloudEurekaMain7001 {
     public static void main(String[] args) {
-        SpringApplication.run(OrderMain80.class,args);
+        SpringApplication.run(CloudEurekaMain7001.class,args);
     }
 }
