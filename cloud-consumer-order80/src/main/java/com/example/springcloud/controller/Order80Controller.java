55a56c0bf581dc4a8d5936b7f03284706d174ac8
zuo_h <zuo_hang97@163.com>
2021-04-13 12:16:19 +0800

    项目重构（提取重复代码）

--- cloud-consumer-order80/src/main/java/com/example/springcloud/controller/Order80Controller.java
+++ cloud-consumer-order80/src/main/java/com/example/springcloud/controller/Order80Controller.java
@@ -17,16 +17,17 @@ import javax.annotation.Resource;
 @RestController
 @Slf4j
 public class Order80Controller {
-    public static final String PAYMENT_URL="http://127.0.0.1:8001";
+    public static final String PAYMENT_URL = "http://127.0.0.1:8001";
     @Resource
-RestTemplate restTemplate;
+    RestTemplate restTemplate;
+
     @GetMapping(value = "/consumer/payment/create")
-    public CommonResult<Payment> creat(Payment payment){
-        return restTemplate.postForObject(PAYMENT_URL+"/payment/create",payment,CommonResult.class);
+    public CommonResult<Payment> creat(Payment payment) {
+        return restTemplate.postForObject(PAYMENT_URL + "/payment/create", payment, CommonResult.class);
     }
 
     @GetMapping(value = "/consumer/payment/getPaymentById/{id}")
-    public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id){
-        return restTemplate.getForObject(PAYMENT_URL+"/payment/getPaymentById/"+id,CommonResult.class);
+    public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id) {
+        return restTemplate.getForObject(PAYMENT_URL + "/payment/getPaymentById/" + id, CommonResult.class);
     }
 }
