e39cd72e7275991a935c781df0675a2aba73e543
zuo_h <zuo_hang97@163.com>
2021-04-13 22:51:18 +0800

    eureka 完结

--- cloud-provider-payment8001/src/main/java/com/example/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/example/springcloud/controller/PaymentController.java
@@ -4,12 +4,16 @@ import com.example.springcloud.entities.CommonResult;
 import com.example.springcloud.entities.Payment;
 import com.example.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @Author zuo_h
@@ -18,12 +22,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @Slf4j
 public class PaymentController {
-    @Autowired
+    @Resource
     private PaymentService paymentService;
 
     @Value("${server.port}")
     private String serverPort;
 
+    /**
+     * 注册后的一些基本信息
+     */
+    @Resource
+    private DiscoveryClient discoveryClient;
+
+    /**
+     * 新增
+     * @param payment
+     * @return
+     */
     @PostMapping(value = "/payment/create")
     public CommonResult create(Payment payment) {
         int i = paymentService.create(payment);
@@ -35,6 +50,11 @@ public class PaymentController {
         }
     }
 
+    /**
+     * 查询
+     * @param id
+     * @return
+     */
     @GetMapping(value = "/payment/getPaymentById/{id}")
     public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id) {
         Payment payment = paymentService.getPaymentById(id);
@@ -44,5 +64,23 @@ public class PaymentController {
         } else {
             return new CommonResult(444, "没有对应记录，查询id为：" + id + serverPort);
         }
+    }
+
+    /**
+     * 利用discoveryClient查看服务信息
+     * @return
+     */
+    @GetMapping(value = "/payment/discovery")
+    public Object discovery() {
+        List<String> services = discoveryClient.getServices();
+        for (String str : services) {
+            log.info("**********element:" + str);
+        }
+        //获取一个微服务下的所有实例
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PROVIDER-SERVICE");
+        for (ServiceInstance instance : instances) {
+            log.info("id:" + instance.getServiceId() + "\thost:" + instance.getHost() + "\tport:" + instance.getPort() + "\turi:" + instance.getUri());
+        }
+        return this.discoveryClient;
     }
 }
